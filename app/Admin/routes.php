fd747d8b9a1d57c0730616e95b631480f0b3797e
parker.fu <parker.fu@feisu.com>
2019-12-12 18:55:47 +0800

    后台产品管理

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -11,5 +11,8 @@ Route::group([
 ], function (Router $router) {
 
     $router->get('/', 'HomeController@index')->name('admin.home');
-
+    $router->get('users', 'UsersController@index');
+    $router->get('products', 'ProductsController@index');
+    $router->get('products/create', 'ProductsController@create');
+    $router->post('products', 'ProductsController@store');
 });
