1ab41e22400421a6d63aeda9ab3900835cc991f9
parker.fu <parker.fu@feisu.com>
2019-12-13 17:07:33 +0800

    前台 商品列表

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,10 @@
 // });
 
 Route::get('/', 'PagesController@root')->name('root')->middleware('verified');
+
+// Route::redirect('/', '/products')->name('root');
+Route::get('products', 'ProductsController@index')->name('products.index');
+
 Auth::routes(['verify' => true]);
 
 Route::get('/home', 'HomeController@index')->name('home');
