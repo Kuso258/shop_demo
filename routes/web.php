2fbc482f6dd5927641464ac6ac315a9354294023
parker.fu <parker.fu@feisu.com>
2019-12-13 18:45:15 +0800

    前台 商品收藏

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,7 @@ Route::get('/', 'PagesController@root')->name('root')->middleware('verified');
 
 // Route::redirect('/', '/products')->name('root');
 Route::get('products', 'ProductsController@index')->name('products.index');
+Route::get('products/{product}', 'ProductsController@show')->name('products.show');
 
 Auth::routes(['verify' => true]);
 
@@ -32,10 +33,14 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 // auth 中间件代表需要登录，verified中间件代表需要经过邮箱验证
 Route::group(['middleware' => ['auth', 'verified']], function() {
+    //用户地址
     Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
     Route::get('user_addresses/create','UserAddressesController@create')->name('user_addresses.create');
     Route::post('user_addresses','UserAddressesController@store')->name('user_addresses.store');
     Route::get('user_addresses/{user_address}', 'UserAddressesController@edit')->name('user_addresses.edit');
     Route::put('user_addresses/{user_address}', 'UserAddressesController@update')->name('user_addresses.update');
     Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
+    //收藏产品
+    Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
+    Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
 });
