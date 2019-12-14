d12fe49a8dbb6d20f1c18ababc66487c6b89f456
parker.fu <parker.fu@feisu.com>
2019-12-14 12:47:11 +0800

    shopping cart

--- database/migrations/2019_12_14_023117_create_cart_items_table.php
+++ database/migrations/2019_12_14_023117_create_cart_items_table.php
@@ -15,7 +15,11 @@ class CreateCartItemsTable extends Migration
     {
         Schema::create('cart_items', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->timestamps();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->unsignedBigInteger('product_sku_id');
+            $table->foreign('product_sku_id')->references('id')->on('product_skus')->onDelete('cascade');
+            $table->unsignedInteger('amount');
         });
     }
 
